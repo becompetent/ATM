6727dc73ae20fbda2a546b4d3adcdb4056ed22ab
yellowpop <knmhappiness@gmail.com>
2025-01-09 17:07:33 +0900

    🐛 입력 검증 및 에러 처리 추가
    잘못 입력된 값을 처리하도록 기능 추가, 유효하지 않은 메뉴 선택시 경고 메시지 출력

--- ATM.py
+++ ATM.py
@@ -16,6 +16,9 @@
 # 단, 영수증에 내역은 변경되지 않아야 하며 입금 또는 출금이 진행될때마다 이력이 기록됩니다.
 # 영수증 변수는 : receipts
 
+# 입력 검증 및 에러 처리 추가
+# 잘못된 입력 값(숫자가 아닌 값, 음수 값 등)을 처리하도록 기능을 추가해주세요
+# 유효하지 않은 메뉴 선택시 경고 메세지 또는 사용방법 재안내를 해주세요 elif 로 구조화 하면 대처하기 쉬움
 
 receipts = [ ] #[ ] 대괄호: list / {} 중괄호 : dict / () :tuple 
 balance = 3000  #현재 잔액을 보여주세요
@@ -24,10 +27,13 @@ while True:
     if num == '4':
         break
     if num=="1":
-       deposit_amount = int(input("입금할 금액을 입력해주세요 : ")) #내장함수: input()/ int()
-       balance += deposit_amount #산술할당 연산자
+       deposit_amount = int(input("입금할 금액을 입력해주세요 : ")) #내장함수: input()/ int() input()>문자로인식
+       if deposit_amount.isdigit() and int(deposit_amount) > 0: #1000 -> True  천원 -> False #Flase 조건을 and 앞에 해주면 코딩속도가 빨라짐
+         balance += int(deposit_amount) #산술할당 연산자
        receipts.append(("입금", deposit_amount, balance))
        print(f"입금하신 금액은 {deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.")
+    else:
+        print("입금한 금액을 숫자 형태와 음수형태가 아닌 겂을 입력해주세요")
 
     if num=="2":
        withdraw_amount = int(input("출금할 금액을 입력해주세요 : ")) 
